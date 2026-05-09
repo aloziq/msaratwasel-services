91f80f2acb37038d1a641848585e0c3a2979eff8
f2363692-commits (f2363692-commits) <f2363692@gmail.com>
2026-05-10 00:26:00 +0300

    Update: api_config.dart with local IP and image path formatting

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -4,8 +4,8 @@ class ApiConfig {
 
   // رابط السيرفر المحلي (تأكد من IP جهازك إذا كنت تختبر على هاتف حقيقي بدلاً من 10.0.2.2)
   static const String _localDomain = "http://192.168.8.188:8001";
-  static const String _localUrl = "$_localDomain/api/"; 
-  
+  static const String _localUrl = "$_localDomain/api/";
+
   // رابط الاستضافة الحية
   static const String _productionDomain = "https://masaratwasal.com/";
 
@@ -17,14 +17,15 @@ class ApiConfig {
   static String get domainUrl => isLocal ? _localDomain : _productionDomain;
 
   static String getImageUrl(String? path) {
-    if (path == null || path.isEmpty) return "https://ui-avatars.com/api/?name=User&background=random&format=png";
+    if (path == null || path.isEmpty)
+      return "https://ui-avatars.com/api/?name=User&background=random&format=png";
     if (path.startsWith('http')) return path;
     if (path.startsWith('/')) {
       path = path.substring(1);
     }
     // Check if the path already contains 'storage/', if not prepend it
     if (path.startsWith('storage/')) {
-        return "$domainUrl/$path";
+      return "$domainUrl/$path";
     }
     return "$domainUrl/storage/$path";
   }
