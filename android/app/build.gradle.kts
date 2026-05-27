99f2fa56b08492be463ad326dad0ab334f21b2bf
f2363692-commits (f2363692-commits) <f2363692@gmail.com>
2026-05-27 20:12:04 +0300

    fix: override compileSdk to 35 in build.gradle.kts to solve connectivity_plus compilation issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.msaratwasel.services"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "28.2.13676358"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
